07b91b7e77ce5691a7d4f126edea44f87e843625
Davi Almeida <davi.castelo.branco.almeida@gmail.com>
2024-07-19 15:06:48 +0100

    started header and applied colours

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
+import logo from '../assets/logo.png'
+
 import styles from './Header.module.css'
 
 export function Header() {
   return (
     <div className={styles.header}>
+      <img 
+        src={ logo } 
+        alt="To-do text with rocket symbol next to it."
+      />
     </div>
   )
 }
